ae974db3516b67bed6ea87ac75e156c3adbcd088
sabikunnaharsafa11 <sabiunnaharsafa92@gmail.com>
2021-08-27 16:31:32 +0600

    first commit

---
+++ js/json.js
@@ -0,0 +1,18 @@
+const user = {id: 11, name: 'safa', address: 'barisal', job:'programer'}
+// console.log(user);
+
+const stringed = JSON.stringify(user);
+// console.log(stringed);
+const stringname = JSON.parse(stringed);
+console.log(stringname);
+
+
+
+
+
+
+
+
+
+
+
